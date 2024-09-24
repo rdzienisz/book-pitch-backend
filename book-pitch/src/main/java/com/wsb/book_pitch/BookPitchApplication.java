a37624d4ca73a755d1857e2245077410436cf979
rdzienisz <dzienisz.robert@gmail.com>
2024-09-24 20:53:58 +0200

    Unpacked spring boot

---
+++ book-pitch/src/main/java/com/wsb/book_pitch/BookPitchApplication.java
@@ -0,0 +1,13 @@
+package com.wsb.book_pitch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookPitchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookPitchApplication.class, args);
+	}
+
+}
---
+++ book-pitch/src/test/java/com/wsb/book_pitch/BookPitchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wsb.book_pitch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookPitchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
