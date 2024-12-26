2facf54d48b7e829a805cb0f42e2352f012d5b34
rdzienisz <dzienisz.robert@gmail.com>
2024-12-26 22:20:18 +0100

    Fixed checkstyle issues

--- book-pitch/src/main/java/com/wsb/book_pitch/controller/UserController.java
+++ book-pitch/src/main/java/com/wsb/book_pitch/controller/UserController.java
@@ -10,7 +10,13 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -34,7 +40,7 @@ public class UserController {
         LocalDateTime startTime = bookingRequest.getStartTime();
         int durationHours = bookingRequest.getDurationHours();
 
-     return bookingService.createBooking(pitchId, email, startTime, durationHours);
+        return bookingService.createBooking(pitchId, email, startTime, durationHours);
     }
 
     @GetMapping("/bookings/{pitchId}")
@@ -46,7 +52,8 @@ public class UserController {
     public ResponseEntity<List<TimeSlot>> checkAvailability(
             @PathVariable Long pitchId,
             @RequestParam String date) {
-        List<TimeSlot> availableSlots = bookingService.checkAvailability(pitchId, LocalDate.parse(date));
+        List<TimeSlot> availableSlots = bookingService.checkAvailability(pitchId,
+                LocalDate.parse(date));
         return ResponseEntity.ok(availableSlots);
     }
 }
--- book-pitch/src/main/java/com/wsb/book_pitch/service/EmailService.java
+++ book-pitch/src/main/java/com/wsb/book_pitch/service/EmailService.java
@@ -1,6 +1,9 @@
 package com.wsb.book_pitch.service;
 
-import com.sendgrid.*;
+import com.sendgrid.Method;
+import com.sendgrid.Request;
+import com.sendgrid.Response;
+import com.sendgrid.SendGrid;
 import com.sendgrid.helpers.mail.Mail;
 import com.sendgrid.helpers.mail.objects.Content;
 import com.sendgrid.helpers.mail.objects.Email;
